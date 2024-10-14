26dedda361f4cee1c51a86bc712dcd696b778a87
banhaum <rafaelbanhos@gmail.com>
2024-10-14 13:51:07 +0100

    add widget test

---
+++ test/widget/home_screen_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_integration_test/app.dart';
+import 'package:flutter_integration_test/screens/home_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Increment Counter on HomePage', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App());
+
+    expect(find.byType(HomeScreen), findsOneWidget);
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.plus_one));
+    await tester.tap(find.byIcon(Icons.plus_one));
+    await tester.pump();
+
+    expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsOneWidget);
+  });
+
+  testWidgets('Change Title on HomePage', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App());
+
+    var titulo =
+        find.byKey(const Key('titulo')).evaluate().single.widget as Text;
+
+    expect(find.byType(HomeScreen), findsOneWidget);
+    expect(titulo.data, equals('Home'));
+
+    await tester.enterText(find.byKey(const Key('tituloInput')), 'Home Page');
+    await tester.pumpAndSettle();
+
+    titulo = find.byKey(const Key('titulo')).evaluate().single.widget as Text;
+    expect(titulo.data, equals("Home Page"));
+  });
+}
